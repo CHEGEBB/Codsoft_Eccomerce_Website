39aa759362c2a9b150060a9a821f813596c926ac
CHEGEBB <chegephil24@gmail.com>
2024-05-26 10:36:01 +0300

    new

--- frontend/src/pages/user.js
+++ frontend/src/pages/user.js
@@ -2,10 +2,15 @@
 const user = () => {
 
     const getLoggedInEmail = () => {
+
+        const email = localStorage.getItem('email');
+        return email;
+    
     }
     return ( 
         <div className="user-account">
             <h1>User Account</h1>
+            
 
 
         </div>
